3917ebdcdb3b7942530d4d22a095379e5fb010e9
Matías Cárdenas <matiascardenas@Matiass-MacBook-Air.local>
2021-09-29 23:39:21 +0200

    Adding Players client

---
+++ src/api/players_client.py
@@ -0,0 +1,20 @@
+from typing import Any, Dict
+
+from src.api.base_client import BaseClient
+from src.request import APIRequest
+
+
+class PlayersClient(BaseClient):
+    def __init__(self) -> None:
+        super().__init__()
+        self.endpoint = "/v3/players"
+        self.request = APIRequest()
+
+    def get_players_stats_by(self, season: int, player_id: int) -> Dict[str, Any]:
+        params = {
+            "season": season,
+            "id": player_id
+        }
+        url = f"{self.base_url}{self.endpoint}"
+
+        return self.request.get(url, params, self.headers)
