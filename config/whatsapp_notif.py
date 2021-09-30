8f1a30a192c5f104fc078b5e7073ddb764650891
Matías Cárdenas <matiascardenas@Matiass-MacBook-Air.local>
2021-10-01 00:07:18 +0200

    Adding whatsapp twilio configuration file

---
+++ config/whatsapp_notif.py
@@ -0,0 +1,12 @@
+
+TWILIO_ACCOUNT_SID = "your-twilio-account-sid"
+TWILIO_ACCOUNT_TOKEN = "your-twilio-account-token"
+
+TWILIO_WHATSAPP_NUMBER = "your-twilio-sandbox-number"
+
+RECIPIENTS = {
+    # Add all your whatsapp recipients as dictionary key/value items in following format:
+    # "Recipient name": "Recipient Number"
+    #
+    # Eg: "John Power": "+1552122545",
+}
