6d91e310a4bc0d76e12deec76007fbaab9a673c3
Matías Cárdenas <matiascardenas@Matiass-MacBook-Air.local>
2021-09-30 18:10:09 +0200

    Adding emojis module

---
+++ src/emojis.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+import emoji
+
+
+class Emojis(Enum):
+    FRANCE = emoji.emojize(":France:", use_aliases=True)
+    ARGENTINA = emoji.emojize(":Argentina:", use_aliases=True)
+    GOAT = emoji.emojize(":goat:", use_aliases=True)
+    THOUGHT_BALLOON = emoji.emojize(":thought_balloon:", use_aliases=True)
+    WAVING_HAND = emoji.emojize(":wave:", use_aliases=True)
+    ALARM_CLOCK = emoji.emojize(":alarm_clock:", use_aliases=True)
+    EARTH = emoji.emojize(":globe_showing_Americas:", use_aliases=True)
+    SOCCER_BALL = emoji.emojize(":soccer:", use_aliases=True)
+    TROPHY = emoji.emojize(":trophy:", use_aliases=True)
+    PUSHPIN = emoji.emojize(":pushpin:", use_aliases=True)
+    SCALE = emoji.emojize(":balance_scale:", use_aliases=True)
+    GLOBE = emoji.emojize(":globe_with_meridians:", use_aliases=True)
+    EUROPEAN_UNION = emoji.emojize(":European_Union:", use_aliases=True)
