2aa3995df1312bebd5b3d54743fd851c4b2f74d9
Matías Cárdenas <matiascardenas@Matiass-MacBook-Air.local>
2021-10-23 14:18:58 +0200

    fix: fixing tests

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,13 +1,12 @@
-from datetime import datetime
-import pytz
 import os
+from datetime import datetime
 
+import pytz
 from freezegun import freeze_time
 
 from src.utils.date_utils import get_date_spanish_text_format
-from src.utils.fixtures_utils import get_next_fixture, date_diff
+from src.utils.fixtures_utils import date_diff, get_next_fixture
 from tests.utils.sample_data_utils import get_sample_data_response
-
 
 TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(TESTS_DIR, "data")
@@ -23,17 +22,20 @@ def test_get_next_fixture():
         next_fixture = get_next_fixture(fixture_response)
 
     # then
-    assert next_fixture.utc_date == datetime.strptime(f'2021-09-29T18:45:00+00:00'[:-6], "%Y-%m-%dT%H:%M:%S")
-    assert next_fixture.home_team == 'Peterborough'
-    assert next_fixture.away_team == 'Bournemouth'
-    assert next_fixture.championship == 'Championship'
-    assert next_fixture.match_status == 'Not Started'
-    assert next_fixture.referee == 'A.  Woolmer'
-    assert next_fixture.round == 'Regular Season - 10'
+    assert next_fixture.utc_date == datetime.strptime(
+        f"2021-09-29T18:45:00+00:00"[:-6], "%Y-%m-%dT%H:%M:%S"
+    )
+    assert next_fixture.home_team == "Peterborough"
+    assert next_fixture.away_team == "Bournemouth"
+    assert next_fixture.championship == "Championship"
+    assert next_fixture.match_status == "Not Started"
+    assert next_fixture.referee == "A.  Woolmer"
+    assert next_fixture.round == "Regular Season - 10"
+
 
 def test_date_diff():
     # given
-    date = '2021-09-30T18:45:00+00:00'
+    date = "2021-09-30T18:45:00+00:00"
 
     # when
     with freeze_time("2021-09-28 18:30:00"):
@@ -45,12 +47,12 @@ def test_date_diff():
 
 def test_date_conversion():
     # given
-    date = '2021-09-29T18:45:00+00:00'
+    date = "2021-09-29T18:45:00+00:00"
     utc_date = datetime.strptime(date[:-6], "%Y-%m-%dT%H:%M:%S")
 
     # when
-    amsterdam_tz = pytz.timezone('Europe/Amsterdam')
-    bsas_tz = pytz.timezone('America/Argentina/Buenos_Aires')
+    amsterdam_tz = pytz.timezone("Europe/Amsterdam")
+    bsas_tz = pytz.timezone("America/Argentina/Buenos_Aires")
 
     local_dt = utc_date.replace(tzinfo=pytz.utc).astimezone(amsterdam_tz)
     bsas_dt = utc_date.replace(tzinfo=pytz.utc).astimezone(bsas_tz)
@@ -65,7 +67,7 @@ def test_date_conversion():
 
 def test_get_date_spanish_text_format():
     # given
-    date = datetime.strptime('2021-10-29T18:45:00', "%Y-%m-%dT%H:%M:%S")
+    date = datetime.strptime("2021-10-29T18:45:00", "%Y-%m-%dT%H:%M:%S")
 
     # when
     formatted_date = get_date_spanish_text_format(date)
--- tests/utils/sample_data_utils.py
+++ tests/utils/sample_data_utils.py
@@ -1,11 +1,11 @@
 import json
 import os
 from pathlib import Path
-from typing import Dict, Any
+from typing import Any, Dict
 
 
 def get_sample_data_response(data_path: Path, json_file) -> Dict[str, Any]:
     json_path = os.path.join(data_path, json_file)
 
-    with open(json_path, mode='r') as f:
+    with open(json_path, mode="r") as f:
         return json.load(f)["response"]
