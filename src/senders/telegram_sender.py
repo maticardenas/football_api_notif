f125c0824aea26c3d5711245cf0868f78a2a5738
Matías Cárdenas <matiascardenas@Matiass-MacBook-Air.local>
2021-11-10 23:00:57 +0100

    fix: fix telegram sender

--- src/senders/telegram_sender.py
+++ src/senders/telegram_sender.py
@@ -5,6 +5,6 @@ from src.api.telegram_client import TelegramClient
 def send_telegram_message(chat_id: str, message: str = "", photo: str = "") -> None:
     telegram_client = TelegramClient(TOKEN)
     if photo:
-        telegram_client.send_photo(chat_id, photo_url=photo)
+        telegram_client.send_photo(chat_id, photo_url=photo, text=message)
     else:
         telegram_client.send_message(chat_id, message)
