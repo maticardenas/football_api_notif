bf85f18751196b5146f6361dd59a0d0f9dd8864a
Matías Cárdenas <matiascardenas@Matiass-MacBook-Air.local>
2021-11-19 00:30:08 +0100

    chore: improving validation at fixture manager
    fix: fixing argentina's record at notification dictionary

--- src/utils/message_utils.py
+++ src/utils/message_utils.py
@@ -15,7 +15,7 @@ def get_team_intro_messages(team_id: str, is_group_notification: bool = False) -
             "next_match": f"{pronoun} recuerdo que el próximo partido del River de Marcelo Gallardios",
             "last_match": f"El River de Marcelo Gallardios",
         },
-        "26:": {
+        "26": {
             "next_match": f"{pronoun} recuerdo que el próximo partido de La Scaloneta {Emojis.ARGENTINA.value}",
             "last_match": f"La Scaloneta {Emojis.ARGENTINA.value}",
         },
--- team_fixture_notifier.py
+++ team_fixture_notifier.py
@@ -1,13 +1,16 @@
 #!/bin/env python
 import sys
-
-from src.senders.whatsapp_sender import send_whatsapp_message
 from src.team_fixtures_manager import TeamFixturesManager
+
 
 if __name__ == "__main__":
     season = sys.argv[1]
     team = sys.argv[2]
+    notif_type = sys.argv[3]
 
     team_fixtures_manager = TeamFixturesManager(season, team)
-    team_fixtures_manager.notify_next_fixture()
-    team_fixtures_manager.notify_last_fixture()
+
+    if notif_type == "next_match":
+        team_fixtures_manager.notify_next_fixture()
+    else:
+        team_fixtures_manager.notify_last_fixture()
