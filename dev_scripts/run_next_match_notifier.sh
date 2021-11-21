86476dbf1c1c6434d6caa4d567d5605ede1a3866
Matías Cárdenas <matiascardenas@Matiass-MacBook-Air.local>
2021-11-21 18:39:38 +0100

    chore: modifying line up update check frequency
    fixing crontab

--- dev_scripts/run_next_match_notifier.sh
+++ dev_scripts/run_next_match_notifier.sh
@@ -7,5 +7,4 @@ cd /usr/football_api
 /usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2021 435 next_match
 /usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2022 435 next_match
 # Argentina
-/usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2021 26 next_match
 /usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2022 26 next_match
