0e756f306fd57defea420bb8cef5f94dcb220093
Matías Cárdenas <matiascardenas@Matiass-MacBook-Air.local>
2022-01-01 13:44:40 +0100

    chore: removing 2021 from scripts execution

--- dev_scripts/run_played_match_notifier.sh
+++ dev_scripts/run_played_match_notifier.sh
@@ -1,10 +1,8 @@
 cd /usr/football_api
 /usr/local/bin/python -m pipenv shell
 # PSG
-/usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2021 85 played_match
 /usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2022 85 played_match
 # River
-/usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2021 435 played_match
 /usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2022 435 played_match
 # Argentina
 /usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2022 26 played_match
