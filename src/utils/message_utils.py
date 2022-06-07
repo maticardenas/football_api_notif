792120064097ff0a5340bbdaade92d0ff39c854b
Matías Cárdenas <matiascardenas@Matiass-MacBook-Air.local>
2022-06-07 18:57:17 +0200

    fix: fixing intro message

--- src/utils/message_utils.py
+++ src/utils/message_utils.py
@@ -9,23 +9,28 @@ TEAMS_ALIASES = {"85": ["PSG"]}
 
 def get_team_intro_messages(team_id: str, is_group_notification: bool = False) -> str:
     pronoun = "Les" if is_group_notification else "Te"
+    first_phrase = "recuerdo que el próximo partido"
     switch = {
         "85": {
-            "next_match": f"{pronoun} recuerdo que el próximo partido del PSG {Emojis.FRANCE.value}"
+            "next_match": f"{pronoun} {first_phrase} del PSG {Emojis.FRANCE.value}"
             f" de Lionel Messi {Emojis.GOAT.value}",
             "last_match": f"El PSG {Emojis.FRANCE.value} de Lionel Messi {Emojis.GOAT.value}",
         },
         "435": {
-            "next_match": f"{pronoun} recuerdo que el próximo partido del River de Marcelo Gallardios",
+            "next_match": f"{pronoun} {first_phrase} del River de Marcelo Gallardios",
             "last_match": f"El River de Marcelo Gallardios",
         },
         "26": {
-            "next_match": f"{pronoun} recuerdo que el próximo partido de La Scaloneta {Emojis.ARGENTINA.value}",
+            "next_match": f"{pronoun} {first_phrase} de La Scaloneta {Emojis.ARGENTINA.value}",
             "last_match": f"La Scaloneta {Emojis.ARGENTINA.value}",
         },
         "451": {
-            "next_match": f"{pronoun} recuerdo el próximo partido de La Battaglieta {Emojis.BLUE_CIRCLE.value}{Emojis.YELLOW_CIRCLE.value}",
+            "next_match": f"{pronoun} {first_phrase} de La Battaglieta {Emojis.BLUE_CIRCLE.value}{Emojis.YELLOW_CIRCLE.value}",
             "last_match": f"La Battaglieta {Emojis.BLUE_CIRCLE.value}{Emojis.YELLOW_CIRCLE.value}",
+        },
+        "440": {
+            "next_match": f"{pronoun} {first_phrase} de Belgrano {Emojis.PIRATE_FLAG.value}",
+            "last_match": f"Belgrano {Emojis.PIRATE_FLAG.value}",
         },
     }
 
