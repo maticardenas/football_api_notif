5b86da755c297d57f647364bcc3e837342251547
Matias Cardenas <cardenasmatias.1990@gmail.com>
2022-06-08 22:51:43 +0200

    fix: fixing script path at dev-scripts

--- dev_scripts/run_played_match_notifier.sh
+++ dev_scripts/run_played_match_notifier.sh
@@ -1,11 +1,11 @@
 cd /usr/football_api
 /usr/local/bin/python -m pipenv shell
 # PSG
-/usr/local/bin/python -m pipenv run python /Users/matiascardenas/Documents/FutbolAPI/team_fixture_notifier.py 2021 85 played_match
-/usr/local/bin/python -m pipenv run python /Users/matiascardenas/Documents/FutbolAPI/team_fixture_notifier.py 2022 85 played_match
+/usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2021 85 played_match
+/usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2022 85 played_match
 # River
-/usr/local/bin/python -m pipenv run python /Users/matiascardenas/Documents/FutbolAPI/team_fixture_notifier.py 2021 435 played_match
-/usr/local/bin/python -m pipenv run python /Users/matiascardenas/Documents/FutbolAPI/team_fixture_notifier.py 2022 435 played_match
+/usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2021 435 played_match
+/usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2022 435 played_match
 # Argentina
-/usr/local/bin/python -m pipenv run python /Users/matiascardenas/Documents/FutbolAPI/team_fixture_notifier.py 2021 26 played_match
-/usr/local/bin/python -m pipenv run python /Users/matiascardenas/Documents/FutbolAPI/team_fixture_notifier.py 2022 26 played_match
+/usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2021 26 played_match
+/usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2022 26 played_match
