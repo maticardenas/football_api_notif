e40a9aaad38259f69098381bf2022942005f048f
Matias Cardenas <cardenasmatias.1990@gmail.com>
2022-06-08 22:51:43 +0200

    chore: putting back 2021 calls as it is current season

--- dev_scripts/run_next_match_notifier.sh
+++ dev_scripts/run_next_match_notifier.sh
@@ -1,8 +1,11 @@
 cd /usr/football_api
 /usr/local/bin/python -m pipenv shell
 # PSG
+/usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2021 85 next_match
 /usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2022 85 next_match
 # River
+/usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2021 435 next_match
 /usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2022 435 next_match
 # Argentina
+/usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2021 26 next_match
 /usr/local/bin/python -m pipenv run python /usr/football_api/team_fixture_notifier.py 2022 26 next_match
