8f921a8f1290fe8c93ac3494cd913c27657efb55
Matias Cardenas <cardenasmatias.1990@gmail.com>
2022-06-08 22:51:42 +0200

    feat: adding standings endpoint call

--- src/api/fixtures_client.py
+++ src/api/fixtures_client.py
@@ -7,11 +7,18 @@ from src.request import APIRequest
 class FixturesClient(BaseClient):
     def __init__(self) -> None:
         super().__init__()
-        self.endpoint = "/v3/fixtures"
         self.request = APIRequest()
 
     def get_fixtures_by(self, season: int, team_id: int) -> Dict[str, Any]:
+        endpoint = "/v3/fixtures"
         params = {"season": season, "team": team_id}
-        url = f"{self.base_url}{self.endpoint}"
+        url = f"{self.base_url}{endpoint}"
+
+        return self.request.get(url, params, self.headers)
+
+    def get_standings_by(self, season: int, team_id: int) -> Dict[str, Any]:
+        endpoint = "/v3/standings"
+        params = {"season": season, "team": team_id}
+        url = f"{self.base_url}{endpoint}"
 
         return self.request.get(url, params, self.headers)
