aef621ca7df88e80f9d77d36c78e62cd511e1740
Matias Cardenas <matias.cardenas@sendcloud.com>
2022-06-13 15:00:50 +0200

    chore: setting proper value to LAST_MATCH_THRESHOLD_DAYS

---
+++ dev_scripts/initialize_database.sh
@@ -0,0 +1,19 @@
+cd /usr/football_api
+/usr/local/bin/python -m pipenv shell
+
+/usr/local/bin/python -m pipenv run python /usr/football_api/db_initializer.py
+
+# PSG
+#/usr/local/bin/python -m pipenv run python /usr/football_api/db_populator.py 2021 85
+#/usr/local/bin/python -m pipenv run python /usr/football_api/db_populator.py 2022 85
+# River
+#/usr/local/bin/python -m pipenv run python /usr/football_api/db_populator.py 2022 435
+## Argentina
+#/usr/local/bin/python -m pipenv run python /usr/football_api/db_populator.py  2022 26
+## Boke
+#/usr/local/bin/python -m pipenv run python /usr/football_api/db_populator.py  2022 451
+## Belgrano
+#/usr/local/bin/python -m pipenv run python /usr/football_api/db_populator.py  2022 440
+## Manchester City
+/usr/local/bin/python -m pipenv run python /usr/football_api/db_populator.py  2021 50
+/usr/local/bin/python -m pipenv run python /usr/football_api/db_populator.py  2022 50
