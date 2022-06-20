626bd0cb64e2b206a8fec1dffe8bbdc009b87de3
Matias Cardenas <matias.cardenas@sendcloud.com>
2022-06-20 14:10:52 +0200

    fix: fixing test, class variable needs to be reset as test pre-conditions, as it is instantiated before in other tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,9 +3,10 @@ from unittest.mock import MagicMock, patch
 from src.db.db_manager import NotifierDBManager
 
 
-@patch("db.db_manager.create_engine")
+@patch("src.db.db_manager.create_engine")
 def test_unique_db_engine_when_multiple_instances_calls(create_engine_mock):
     # given
+    NotifierDBManager.ENGINE = None
     create_engine_mock.side_effect = [MagicMock(id="mock_1"), MagicMock(id="mock_2")]
 
     # when - then
